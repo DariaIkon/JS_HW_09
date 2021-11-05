1de776ddc6af588bae36810fa371b63bcdbd7ee8
DariaIkon <ikonnikova.daria@gmail.com>
2021-11-05 18:02:10 +0300

    done

--- main.js
+++ main.js
@@ -1,28 +1,22 @@
 window.onload = function () {
 
 // В поле "Full Name" запретите вводить цифры.
-    var inputFullName = document.getElementById('full_name');
-    inputFullName.keypress = (() => {
+    document.getElementById('full_name').onkeypress = ((event) => {
         var number = parseInt(event.key);
-    if (!isNaN(number)) {
-        event.preventDefault();
-    } else {
-        console.log();
-    }
+            if (!isNaN(number)) {
+                event.preventDefault();
+            }
     });
 // В поле "Your username" запретите вводить точки и запятые.
-    var inputUserName = document.getElementById('username');
-    inputUserName.keypress = (() => {
-    if (event.key === '.' || ',') {
-        inputFullName.preventDefault();
-    } else {
-        console.log(inputUserName.keypress);
-    }
+    document.getElementById('username').onkeypress = ((event) => {
+            if (event.key === '.' || event.key === ',') {
+                event.preventDefault();
+            } 
     });
 // При изменении значения чекбокса выводите в консоль соответствующее предложение: “Согласен” или “Не согласен”.
     var checkbox = document.getElementById('checkbox');
     checkbox.onchange = (() => {
-        if (checkbox.checked = true) {
+        if (checkbox.checked === true) {
             console.log('Согласен');
         } else {
             console.log('Не согласен');
@@ -42,45 +36,65 @@ window.onload = function () {
 // Если код по всем проверкам успешно - вывести “OKAY”, используя alert.
 
     var button = document.getElementsByTagName('button');
-    button.onclick = (() => {
     var inputs = document.getElementsByTagName('input');
-    for (var i = 0; i < inputs.length; i++) {
-    if (inputs.value[i] === undefined) {
-        alert('Заполните поле + inputs[i].id')
-        return;
-    }
-}
-        var password = document.getElementById('password');
-        var repeatPassword = document.getElementById('repeatPassword');
-        if (password !== repeatPassword) {
-            alert('Пароль введен не верно');
+    var password = document.getElementById('password');
+    var repeatPassword = document.getElementById('repeatPassword');
+    button.onclick = ((event) => {
+        if (inputs[0].value === '') {
+            alert('Заполните поле Full Name');
+            return;
         }
-
+        if (inputs[1].value === '') {
+            alert('Заполните поле Your username');
+            return;
+        }
+        if (inputs[2].value === '') {
+            alert('Заполните поле E-mail');
+            return;
+        }
+        if (inputs[3].value === '') {
+            alert('Заполните поле Password');
+            return;
+        }
+        if (inputs[4].value === '') {
+            alert('Заполните поле Repeat Password');
+            return;
+        } 
+        if (password[0].value !== repeatPassword[0].value) {
+            alert('Пароль введен не верно');
+            return;
+        }
+        if (checkbox.checked !== true) {
+            alert('Подтвердите соглашение')
+            return;
+        } 
+        alert('Okey');
+        return;
     });
+       
 
     
-// При нажатии на ссылку "Already have an account?" сделать следующие действия со страницей:
+// При нажатии на ссылку "Already have an account?" 
+// сделать следующие действия со страницей:
 // Текст "Get your free account" заменить на "Log in to the system".
 // Блоки с полями "Full Name", "E-mail", "Repeat Password" удалите.
 // Блок с чекбоксом также удалите.
 // Текст в кнопке замените на "Sign In"
 // Ссылку "Already have an account?" также удалите
    
-    var link = document.getElementsByClassName('form__link');
-    link.onclick = (() => {
-        var inputs = document.getElementsByTagName('input');
-        for (var i = 0; i < inputs.length; i++) {
-            if (inputs.value[i] !== undefined) {
-                inputs[i].remove();
-                return;
-            }
-        }
-        link.innerText = 'Log in to the system';
+    var link = document.getElementById('form__link');
+    var formInputRepeatPassword = document.getElementsByClassName('form__input__wrapper__repeat__password');
+    var formInputFullName = document.getElementsByClassName('form__input__wrapper__full_name');
+    var formInputMail = document.getElementsByClassName('form__input__wrapper__mail');
+    var formInputCheckbox = document.getElementsByClassName('form__input__wrapper__checkbox');
+    var title = document.getElementsByClassName('form__title');
+    link.onclick = ((event) => {
+        formInputRepeatPassword.remove();
+        formInputFullName.remove();
+        formInputMail.remove();
+        formInputCheckbox.remove();
+        title.innerText = 'Log in to the system';
         button.innerText = 'Sign In';
         link.remove();
     });
-
-
-   
-
  };
